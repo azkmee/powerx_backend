cbbd0c1b72df4135e6094754959749d098ab327e
azkmee <azmi_aang@hotmail.com>
2021-10-06 08:25:05 +0800

    get list all testing

--- capstone/test/todo.int.test.js
+++ capstone/test/todo.int.test.js
@@ -17,26 +17,61 @@ afterAll(async () => {
     await utils.teardown()
 })
 
-// describe('GET /lists/all', () => {
-//     describe('given no lists', () => {
-//         beforeAll(async() => {
-//             await db.clearListsTable()
-//         })
-//         it('should return empty', async () => {
+describe('GET /lists/all', () => {
+    describe('given no lists', () => {
+        beforeAll(async() => {
+            await db.clearListsTable()
+        })
+        const expectedRes = {}
+        it('should return empty', async () => {
+            return request(app)
+                .get('/lists/all')
+                .set('Authorization', token1)
+                .expect(200)
+                .then(res => {
+                    expect(res.body).toEqual(expectedRes)
+                })
+        })
+    })
 
-//         })
-//     })
+    describe('given some items in db', () => {
+        const list1 = {
+            name:'Test List1',
+            items:['test item 11','test item 12']
+        }
+        const list2 = {
+            name:'Test List2',
+            items:['test item 21','test item 22']
+        }
+        beforeAll(async () => {
+            await db.clearListsTable()
+            await request(app)
+                .post('/lists')
+                .set('Authorization', token1)
+                .send({...list1})
+            await request(app)
+                .post('/lists')
+                .set('Authorization', token1)
+                .send({...list2})
+        })
 
-//     describe('given some items in db', () => {
-//         const list = {
+        it('should return names of list', async () => {
+            return request(app)
+                .get('/lists/all')
+                .set('Authorization', token1)
+                .expect(200)
+                .then(res => {
+                    expect(res.body).toEqual(
+                        expect.arrayContaining(
+                            [list1, list2].map((item, index) => {
+                                return { id: index+1, name: item.name}
+                            })
+                        )
+                    )
+                })
+        })
+    })
 
-//         }
-//         beforeAll(async () => {
-//             await db.clearListsTable()
-//             await Promise.all
-//         })
-//     })
 
-
-//     describe('given a ')
-// })
+    // describe('given a ')
+})
